98b71ee4652308e925d5a95998225e682e76407b
RaEMT-ErWu <3689505834@qq.com>
2026-04-21 23:40:45 +0800

    Update test_game.py

--- tests/test_game.py
+++ tests/test_game.py
@@ -9,17 +9,15 @@ import constants as c
 
 class TestMazeGameTDD(unittest.TestCase):
     def setUp(self):
-        # 初始化游戏
         self.maze = MazeMap()
         self.player = Player()
         self.cmd = CommandHandler(self.maze, self.player)
 
-    # 测试 1：玩家不能走出地图边界
-    def test_player_cannot_move_out_of_border(self):
-        # 玩家初始位置 (1,1)
+    # 测试 1：撞墙不会移动
+    def test_player_cannot_move_into_wall(self):
+        # 玩家初始位置 (1,1)，往上是墙，移动无效
         self.cmd.handle_move("w")
-        self.cmd.handle_move("w")
-        self.assertEqual(self.maze.player_y, 0)  # 不会再动
+        self.assertEqual(self.maze.player_y, 1)  # 位置不变
 
     # 测试 2：玩家可以正常拾取物品 K
     def test_player_pick_item(self):
